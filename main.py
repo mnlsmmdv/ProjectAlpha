fd4bc4a0f77a436256eb534f592b046d7e225e66
Ahmed Affaan <71839282+mnlsmmdv@users.noreply.github.com>
2022-11-03 20:24:51 +0500

    Window button and screen centering

--- main.py
+++ main.py
@@ -21,5 +21,17 @@ splash_screen.geometry("800x608") # Window dimensions.
 splash_screen.resizable(False, False) # Keeping constant window dimension size.
 splash_screen.configure(bg="#191414")
 
+# Configurations to center splash screen window on initial run.
+splash_screen.update() # Refreshes the window.
+splash_screen_width = splash_screen.winfo_width() # Retrieves the window width.
+splash_screen_height = splash_screen.winfo_height() # Retrieves the window height.
+screen_width = splash_screen.winfo_screenwidth() # Retrieves the screen width.
+screen_height = splash_screen.winfo_screenheight() # Retrieves the screen height.
+x = int((screen_width / 2) - (splash_screen_width / 2)) # Calculates x-axis.
+y = int((screen_height / 2) - (splash_screen_height / 2)) # Calculates y-axis.
+splash_screen.geometry(f"{splash_screen_width}x{splash_screen_height}+{x}+{y}") # Sets the window size to any screen center.
+splash_screen_button1 = Button(splash_screen, text="START REQUESTING", font=("consolas bold", 11), bg="#1DB954", fg="#FFFFFF", width=5, height=2) # Button for splash screen.
+splash_screen_button1.place(x=335, y=410, width=145) # Button placement.
+
 # Loops all windows.
 mainloop()
