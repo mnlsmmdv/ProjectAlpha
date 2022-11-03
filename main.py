97c4ca15591086f96f8e75e332cbb6adf1c9a51d
Ahmed Affaan <71839282+mnlsmmdv@users.noreply.github.com>
2022-11-03 20:05:29 +0500

    Displays splash screen

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ Note: Uncomment codes to execute and comment them when not in use.
 
 # Importing libraries.
 from tkinter import *
+
+# Splash screen window configurations.
+splash_screen = Tk()
+splash_screen.title("Project Alpha") # Window title.
+splash_screen.geometry("800x608") # Window dimensions.
+splash_screen.resizable(False, False) # Keeping constant window dimension size.
+splash_screen.configure(bg="#191414")
+
+# Loops all windows.
+mainloop()
