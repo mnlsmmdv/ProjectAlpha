f5fbfebd75fb79647eebbadd6378c543e4abf9d6
Ahmed Affaan <71839282+mnlsmmdv@users.noreply.github.com>
2022-11-03 20:01:08 +0500

    File description set

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Name: Ahmed Affaan
+Title: main.py
+Folder: ProjectAlpha
+Date: 03/11/2022
+Country: Republic of Maldives
+Code version: -
+Description:
+Credits to:
+Credits to link: 
+Note: Uncomment codes to execute and comment them when not in use.
+"""
